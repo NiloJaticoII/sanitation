ec98a75e13fe442b42cee24f9e5c547c1b359697
Nilo Cantil II Jatico <nilo.jaticoii@gmail.com>
2022-09-05 16:01:39 +0800

    Added constructor, methods and attributes for Person

---
+++ Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanitation
+{
+    public class Person
+    {
+        private string name;
+        private bool hygiene;
+
+        public Person(string name, bool hygiene)
+        {
+            this.name = name;
+            this.hygiene = hygiene;
+        }
+
+        public bool Hygiene
+        {
+            get { return this.hygiene; }
+            set { this.hygiene = value; }
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public void Play()
+        {
+            this.hygiene = false;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Sanitation;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
